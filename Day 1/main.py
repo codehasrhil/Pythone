ff88baaf641e83f67bd7190c247191e801739a74
codehasril <harshilRana504@gmail.com>
2026-01-19 21:21:07 +0530

    Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,39 @@
+                                     #Data Types
+#Variable = A container for a value (string, integer,float,boolean)
+#           A variable behaves as if it was the value it contains
+
+#String = A series of characters
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"YOur email is {email}")
+
+#Integer = A whole number
+age = 23
+quaintity = 3
+people = 5
+
+
+print(f"You are {age} years old")
+print(f"You want to buy {quaintity} of pizza")
+
+#float = A number with a decimal
+
+price = 10.99
+gpa = 3.2
+distance = 1.5
+
+print(f"The price is {price}")
+print(f"Your gpa is {gpa}")
+print(f"You run {distance} km away")
+
+
+# boolean = A True or false value
+human = True
+isStudent = False
+
+print(f"are you a human? {human}")
+print(f'is i am student? {isStudent}')
