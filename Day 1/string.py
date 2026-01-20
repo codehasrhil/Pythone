f662fc63b68987bf4cd6c4ca1fb9e9cd01f9d2ec
codehasril <harshilRana504@gmail.com>
2026-01-20 22:11:48 +0530

    Day 1

---
+++ Day 1/string.py
@@ -0,0 +1,23 @@
+#String = A string is a sequence of characters written inside quotes.
+
+name  = "Harshil"
+Surname = "Rana"
+Towan = "Dholka"
+
+print(f"My name is {name} {Surname}")
+
+print(f"i live in {Towan}")
+
+
+"""Length = use to get length of string"""
+
+print(len(name))
+
+"""UpperCase = use to make string uppercase"""
+
+print(name.upper())
+
+"""LowerCase = use to make string lowerCase"""
+
+print(name.lower())
+
