6d6b4fb22dd71361f5b1434693c56ecae33ead9d
codehasril <harshilRana504@gmail.com>
2026-01-28 22:07:35 +0530

    Day 1

---
+++ Day 1/number.py
@@ -0,0 +1,30 @@
+# Types of number of python :- there are 2 type of numbers in python
+
+"1 int = whole number not decimal"
+
+a = 10
+b = 5
+
+print(f"{a},{b}")
+
+"2 float = numbers with the decimal"
+
+price = 99.50
+pi = 3.14
+
+print(f"{price},{pi}")
+
+
+# Basic Arithmetic Operators
+
+print(f"{a} + {b} =",a+b);  # + = addition
+print(f"{a} - {b} =",a-b);  # - = subtraction
+print(f"{a} * {b} =",a*b);  # * = Multipliccation
+print(f"{a} / {b} =",a/b);  # / = Division
+ 
+# Special Operators (very Important)
+
+print(f"{a} // {b} =",a//b); # // give whole number , removes decimal
+print(f"{a} %  {b} =",a%b);  # %  use to find the rimainder
+print(f"{a} ** {b} =",a**b); # ** use to exponent the power
+
