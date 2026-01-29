8ab0cf397fae196314a2d78ec2431093a322b8c0
codehasril <harshilRana504@gmail.com>
2026-01-29 22:29:38 +0530

    Day 1

--- Day 1/number.py
+++ Day 1/number.py
@@ -28,3 +28,35 @@ print(f"{a} // {b} =",a//b); # // give whole number , removes decimal
 print(f"{a} %  {b} =",a%b);  # %  use to find the rimainder
 print(f"{a} ** {b} =",a**b); # ** use to exponent the power
 
+#Operator Precdence = 1 = () if you put any math inside this bracket it become important so it execute first
+#                     2 = ** power is the second one
+#                     3 = * , / , // , %  then this all execute in left to right
+
+
+#Type Conversion
+
+"int = it cuts the value after .(decimal)"
+
+num = int(3.8)
+print(num)
+
+"float = add .0 after any value"
+
+n = float(4)
+print(n)
+
+"str = use for converting num into text"
+
+s = str(100)
+print(s)
+
+#round = make value roundfigure
+
+round = round(3.6)
+print(round)
+
+"abs = make value always positive"
+
+ab = abs(-10)
+print(ab)
+
