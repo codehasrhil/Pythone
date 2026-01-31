faf89445c57c8041dfb9b2931e33eaaa6c9dc570
codehasril <harshilRana504@gmail.com>
2026-01-31 20:40:50 +0530

    Day-1

---
+++ Day 1/in&out.py
@@ -0,0 +1,21 @@
+print("hello world") # Basic output
+
+"sep"
+
+print("python","java","c++",sep = ' | ')
+
+
+# input = use to take thing from 
+
+name  = input("Enter Your name")
+print("Hello",name)
+
+
+
+#Type converstion is important
+
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print(a + b)
