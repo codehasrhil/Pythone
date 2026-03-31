c371c58b02fdcda400a8dd58f4a1a854d9f264ed
codehasril <harshilRana504@gmail.com>
2026-03-31 13:07:09 +0530

    Day-2

---
+++ Day-2/if&else/if.py
@@ -0,0 +1,48 @@
+#used to make decision based on condition
+
+#syntex
+
+'''
+if condtion:
+    # runs if true
+    
+elif condtion:
+    # runs if above false and this true
+
+else:
+    # runs if all false
+    
+'''
+
+#ex
+
+age = 18
+
+if age < 18:
+    print("minor")
+elif age == 18:
+    print("just adult")
+else:
+    print("adult")
+    
+    
+# there are thing called conditions 
+
+'''
+< = less then   ex- 18<25
+> = greter then ex- 25>24
+>= : greter then and ecule to ex:- 25>=12 or 23>=23
+<= : less then ecule to ex:- 25<=34 or 25<=25
+== : comper value 
+!= : its mean not ecule to 
+
+'''
+
+# logical operators
+
+'''
+and : check both condtion aand both must be true
+or : cany one condtion must be true
+not : work as revers make false to true and true to false
+
+'''
