98f0e759a3472754f1fd7ddace6c433a059e5996
codehasril <harshilRana504@gmail.com>
2026-04-02 18:57:37 +0530

    Day-3

---
+++ Day-3/dectionary/main.py
@@ -0,0 +1,25 @@
+students={
+    "name":"Rana",
+    "marks": 90
+}
+
+print(students['name'])
+
+
+#adding and update
+
+d = {"a":1}
+
+d["b"]  = 2  # to add
+d["a"]  = 100 # to update
+
+print(d)
+
+#methods
+
+d.pop("a")  # use to remove  
+d.popitem() # sue to remove last value
+
+
+print(d)
+
